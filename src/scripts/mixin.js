12e0776d273d4fd199f48c4d440cb2898870cb92
Danilo Diez <daniloo10.dd@gmail.com>
2022-10-28 16:09:17 -0500

    mixin component 😐

---
+++ src/scripts/mixin.js
@@ -0,0 +1,16 @@
+// Add functionality to a obj or class without inheritance
+
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const dogFunctionality = {
+  bark: () => console.log("Woof!"),
+  wagTail: () => console.log("Wagging my tail!"),
+  play: () => console.log("Playing!")
+};
+
+Object.assign(Dog.prototype, dogFunctionality)
